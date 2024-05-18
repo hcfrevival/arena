6dfa945a9b9f7b650a9901d000f371969a8fd86d
j-rockwell <john@llewkcor.com>
2024-05-17 23:26:49 -0700

    main: Remove player from team upon disconnect

--- src/main/java/net/hcfrevival/arena/listener/TeamListener.java
+++ src/main/java/net/hcfrevival/arena/listener/TeamListener.java
@@ -4,14 +4,25 @@ import gg.hcfactions.libs.bukkit.events.impl.PlayerDamagePlayerEvent;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import net.hcfrevival.arena.ArenaPlugin;
+import net.hcfrevival.arena.player.PlayerManager;
 import net.hcfrevival.arena.team.TeamManager;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 @AllArgsConstructor
 public final class TeamListener implements Listener {
     @Getter public final ArenaPlugin plugin;
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        PlayerManager playerManager = (PlayerManager) plugin.getManagers().get(PlayerManager.class);
+        TeamManager teamManager = (TeamManager) plugin.getManagers().get(TeamManager.class);
+
+        playerManager.getPlayer(player.getUniqueId()).ifPresent(arenaPlayer -> teamManager.getTeam(player).ifPresent(team -> team.removeMember(arenaPlayer)));
+    }
 
     @EventHandler
     public void onPlayerDamagePlayer(PlayerDamagePlayerEvent event) {
