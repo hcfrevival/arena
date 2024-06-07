d52ecef3d426cd4a0c776353800a5211f441ec80
j-rockwell <john@llewkcor.com>
2024-06-06 20:39:52 -0700

    1.0.1: Disable weather change

--- src/main/java/net/hcfrevival/arena/listener/WorldListener.java
+++ src/main/java/net/hcfrevival/arena/listener/WorldListener.java
@@ -4,6 +4,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockSpreadEvent;
 import org.bukkit.event.block.LeavesDecayEvent;
+import org.bukkit.event.weather.WeatherChangeEvent;
 
 public final class WorldListener implements Listener {
     @EventHandler
@@ -14,5 +15,13 @@ public final class WorldListener implements Listener {
     @EventHandler
     public void onBlockSpread(BlockSpreadEvent event) {
         event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onWeatherChange(WeatherChangeEvent event) {
+        event.setCancelled(true);
+        event.getWorld().setStorm(false);
+        event.getWorld().setThundering(false);
+        event.getWorld().setThunderDuration(0);
     }
 }
